e549ab6fd00be1acc6b1bc81808c5f1e24a63e4a
Your Name <you@example.com>
2019-01-06 13:10:01 +0900

    item migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190106034853) do
+
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "use_id"
+    t.string   "name"
+    t.string   "image"
+    t.text     "description",  limit: 65535
+    t.string   "status"
+    t.integer  "delivery_fee"
+    t.string   "pre_date"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
 end
