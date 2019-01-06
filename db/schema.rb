c4cd57694cc762eff02b39d4f0978ec7310df66d
Your Name <you@example.com>
2019-01-06 16:41:08 +0900

    Rails g model value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190106034853) do
+ActiveRecord::Schema.define(version: 20190106073510) do
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "use_id"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20190106034853) do
     t.string   "pre_date"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "values", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "item_id"
+    t.integer  "price",      null: false
+    t.integer  "profit",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_values_on_item_id", using: :btree
   end
 
 end
