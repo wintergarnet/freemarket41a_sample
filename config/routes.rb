61a242d15d3a11b17ae510cede22fdb967c45d57
Your Name <you@example.com>
2019-01-08 19:32:22 +0900

    Add user address registration view

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+
   root 'items#index'
   resources :items, only:[:index, :new, :create, :show] do
-    get 'sample'
   end
-  
-  
+
+
   resources :users do
     get :choice, on: :collection
     post :follow, on: :member
