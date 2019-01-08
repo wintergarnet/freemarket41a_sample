d0bc2222109c842bf2d16f0ed8949135b9eccc99
nanashimada <shimadanana@shimadanananoMacBook-puro.local>
2019-01-08 20:14:18 +0900

    Change routes to show mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+
   root 'items#index'
   resources :items, only:[:index, :new, :create, :show] do
     get 'sample'
   end
-  
-  
+
+
   resources :users do
     get :choice, on: :collection
     post :follow, on: :member
   end
 
-  get 'items', to: 'items#mypage'
 end
