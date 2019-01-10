3a0200789c8fa8cf38726626c4f226f8db104dbb
wintergarnet <yuta@yuta-no-MacBook-Air.local>
2019-01-10 16:27:47 +0900

    add file

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def mypage
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+  end
+
+  def show
+    @item = Item.new
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+
+  def edit
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,21 @@ class UsersController < ApplicationController
   def choice
   end
 
+  # def index
+  #   @user = User.new
+  # end
+
+  def edit
+  end
+
   def new
     @user = User.new
   end
 
   def telephone
     @user = User.new
+
+  def show
   end
 
 end
