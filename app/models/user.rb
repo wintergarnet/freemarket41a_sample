02e64afdd842979701e554bb31cc365bab4a4838
nanashimada <shimadanana@shimadanananoMacBook-puro.local>
2019-01-10 17:59:22 +0900

    Change where to put 'validates' line

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
+
   has_many :comments
   has_many :items
   has_many :purchases
   has_one :address
   has_one :birth
   has_one :credit
-
-  validates :nickname, presence: true
 
 end
 
