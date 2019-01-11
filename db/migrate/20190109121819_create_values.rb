9e9d378c9237456a58d24219528f4004e7c3cfd0
nanashimada <shimadanana@shimadanananoMacBook-puro.local>
2019-01-11 13:03:42 +0900

    Fix migrate/20190109121819_create_values.rb

--- db/migrate/20190109121819_create_values.rb
+++ db/migrate/20190109121819_create_values.rb
@@ -1,7 +1,7 @@
-class CreateValues < ActiveRecord::Migration[5.0]
+class CreateValues < ActiveRecord::Migration[4.2]
   def change
     create_table :values do |t|
-      t.references :item, forreign_key: true
+      t.references :item, forreign_key: true unless index_exists?(:values, :item_id)
       t.integer :price, null: false
       t.integer :profit, null: false
       t.timestamps
