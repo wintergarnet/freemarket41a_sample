db5f3b7c8d4219bfcde6e6d92a87651626241945
Your Name <you@example.com>
2019-01-13 20:58:28 +0900

    Add item edit function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :link_user
-  # before_action :set_item, only[:edit, :update]
+  before_action :set_item, only: [:edit, :update]
 
   def index
     @items = Item.where('id >= 1').limit(4)
@@ -27,16 +27,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    #商品詳細ページがないため仮置き
-    #TODO: privateのset_itemに変更する
-    @item = Item.find(1)
     @item.value
   end
 
   def update
-    #商品詳細ページがないため仮置き
-    #TODO: privateのset_itemに変更する
-    @item = Item.find(1)
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -51,7 +45,7 @@ class ItemsController < ApplicationController
 
   def item_params
     #TODO: .merge(user_id: current_user.id)として分ける
-    params.require(:item).permit(:name, :item_condition, :image, :description, :status, :ship_from, :delivery_fee, :pre_date, value_attributes: [:id, :item_id, :price, :profit]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :item_condition, :image, :description, :status, :ship_from, :delivery_fee, :pre_date, :user_id, value_attributes: [:id, :item_id, :price, :profit]).merge(user_id: current_user.id)
   end
 
   def link_user
