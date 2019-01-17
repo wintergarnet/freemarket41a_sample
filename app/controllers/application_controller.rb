d695c5c29d547f08a67835b70f8f7c0543763a06
nanashimada <shimadanana@shimadanananoMacBook-puro.local>
2019-01-17 17:03:22 +0900

    Fix controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
+  # def after_sign_in_path_for(resource)
+  #   user_telephone_path(current_user)
+  # end
+
   private
 
   def production?
