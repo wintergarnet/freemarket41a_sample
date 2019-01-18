b88e0252b9476b7186560352d35b362b5f41b54b
Your Name <you@example.com>
2019-01-18 10:15:12 +0900

    Add autoplay

--- app/assets/javascripts/header-banner.js
+++ app/assets/javascripts/header-banner.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('.owl-carousel').owlCarousel({
     items: 1,
-    autoPlay: 1000,
+    autoplay: 3000,
     loop: true,
     autoplaySpeed: 800,
     navigation: true,
-    singleItem: true
+    singleItem: true,
   });
 });
