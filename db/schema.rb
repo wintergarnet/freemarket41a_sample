c11432e44b1b9151df5678a837fc1509504afab9
wintergarnet <yuta@yuta-no-MacBook-Air.local>
2019-01-20 12:35:01 +0900

    edit morepage other

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20190118094853) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
