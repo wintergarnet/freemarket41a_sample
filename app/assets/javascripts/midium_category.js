e98af698234cf7642b228d0ee58d945bc96a0aa4
Your Name <you@example.com>
2019-01-24 07:33:12 +0900

    update category form

--- app/assets/javascripts/midium_category.js
+++ app/assets/javascripts/midium_category.js
@@ -24,5 +24,29 @@ $(document).ready(function(){
         });
       })
     });
+    $('#q_parent_category_large_category_eq').on('change', function(){
+      var url = '/items/set_midium_category';
+      var data = {
+        category_id: $(this).val()
+      };
+      $.ajax({
+        type: 'GET',
+        url: url,
+        data: data,
+        dataType: 'json'
+      })
+      .done(function(data){
+        $('.search-midium_category').html(`
+        <div class='select-wrapper'>
+          <select class="select-form" name="q[parent_category_midium_category_eq]" id="item_parent_category_midium_category"><option value="">---</option>
+          </select>
+          <i class="fa fa-angle-down icon"></i>
+        </div>`);
+
+        $.each(data, function (i, val) {
+        	$('#item_parent_category_midium_category').append($("<option>").val(val.id).text(val.name));
+        });
+      })
+    });
   });
 });
--- app/assets/javascripts/small_category.js
+++ app/assets/javascripts/small_category.js
@@ -25,5 +25,30 @@ $(document).ready(function(){
         });
       })
     });
+    $('.search-midium_category').on('change', function(){
+      var url = '/items/set_small_category';
+      var midium_category_id = $('#item_parent_category_midium_category').val();
+      var data = {
+        category_id: midium_category_id
+      };
+      $.ajax({
+        type: 'GET',
+        url: url,
+        data: data,
+        dataType: 'json'
+      })
+      .done(function(data){
+        $('.small_category').html(`
+        <div class='select-wrapper'>
+          <select class="select-form" name="q[parent_category_small_category_eq]" id="item_parent_category_small_category"><option value="">---</option>
+          </select>
+          <i class="fa fa-angle-down icon"></i>
+        </div>`);
+
+        $.each(data, function (i, val) {
+        	$('#item_parent_category_small_category').append($("<option>").val(val.id).text(val.name));
+        });
+      })
+    });
   });
 });
